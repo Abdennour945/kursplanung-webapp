a28e1c6bf1ca527b29b327814fc634e7faba6e7e
Abdennour bencharda <abdennour@MacBook-Air-von-Abdennour.local>
2026-06-28 19:17:17 +0200

    Add database example configuration

---
+++ config/db.example.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$dbname = "kursplanung_app";
+$user = "your_username";
+$password = "your_password";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Datenbankfehler: " . $e->getMessage());
+}
